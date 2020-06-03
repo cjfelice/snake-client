e11cff851005860f051c75dbd06b7c7719f0a40d
cjfelice <monocularmedia@gmail.com>
2020-06-03 21:46:34 +0000

    set up connection actions for snek

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.148',
+    port: 50541
+  });
+  
+  conn.on('data', (data) => console.log(data));
+
+  conn.on('connect', () => console.log('you on the plane!'));
+
+  conn.on('connect', () => conn.write('Name: Isz'))
+
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+module.exports = { connect };
