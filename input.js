e7190831118e8e79e1e1576b2ea16d82e7f11329
cjfelice <monocularmedia@gmail.com>
2020-06-03 23:07:56 +0000

    refactoring

---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = function() {
+
+  const stdin = process.stdin;
+  
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  stdin.on('data', handleUserInput)
+  stdin.setRawMode();
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,18 +1,6 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  const handleUserInput = function(key) {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  };
-  stdin.on('data', handleUserInput)
-  stdin.setRawMode();
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-};
 
 setupInput();
 
